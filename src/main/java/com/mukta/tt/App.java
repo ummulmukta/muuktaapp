51ba7c8787763a4dc47e1f5967a00a6094697160
ummulmukta <41453685+ummulmukta@users.noreply.github.com>
2019-03-09 09:35:39 -0500

    commit by mukta

---
+++ src/main/java/com/mukta/tt/App.java
@@ -0,0 +1,13 @@
+package com.mukta.tt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
